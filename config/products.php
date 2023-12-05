4f08498273d05dd72ef3a542ad55c4c3bfbd55e5
Valentina-Web-Developer <valentina.webdev@gmail.com>
2023-12-05 17:38:35 +0100

    pruduct brench with seeder

---
+++ config/products.php
@@ -0,0 +1,378 @@
+<?php
+
+
+
+return [
+
+    // <!-- americano -->
+    [
+        "name" => "hamburger con patatine",
+        "price" => 10,
+        "description" => "Questo hamburger è un connubio perfetto tra il sapore delle carni scelte e le salse.",
+        "ingredients" => ['pane', 'hamburger', 'maionese', 'ketchup', 'patatine', 'lattuga'],
+        "is_available" => true,
+        "cover_image" => "",
+        
+    ],
+    [
+    "name" => "Cheeseburger Classico",
+    "price" => 12,
+    "description" => "Un classico intramontabile americano con carne succulenta, formaggio fuso e la perfetta combinazione di condimenti.",
+    "ingredients" => ['panino', 'polpetta di manzo', 'formaggio cheddar', 'ketchup', 'senape', 'sottaceti'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Panino con Maiale Tirato e Barbecue",
+    "price" => 14,
+    "description" => "Maiale tirato tenero in una ricca salsa barbecue, servito in un morbido panino.",
+    "ingredients" => ['panino', 'maiale tirato', 'salsa barbecue', 'coleslaw'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Ali di Pollo alla Buffalo",
+    "price" => 9,
+    "description" => "Ali di pollo croccanti e piccanti, immerse in una salsa Buffalo agrodolce, perfette da condividere.",
+    "ingredients" => ['ali di pollo', 'salsa Buffalo', 'salsa al formaggio blu', 'sedano'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Pizza Stile New York",
+    "price" => 16,
+    "description" => "Una fetta della Grande Mela con una crosta sottile e pieghevole e deliziosi condimenti.",
+    "ingredients" => ['impasto per pizza', 'salsa di pomodoro', 'formaggio mozzarella', 'salame piccante', 'olive'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Pollo Fritto Stile del Sud",
+    "price" => 18,
+    "description" => "Pollo croccante e dorato con un tocco di spezie del sud.",
+    "ingredients" => ['pezzi di pollo', 'latticello', 'farina', 'spezie', 'purè di patate', 'sugo'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Insalata Cobb Californiana",
+    "price" => 15,
+    "description" => "Un'insalata rinfrescante con pollo alla griglia, avocado, pancetta e formaggio blu.",
+    "ingredients" => ['insalata mista', 'pollo alla griglia', 'avocado', 'pancetta', 'formaggio blu', 'condimento'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+// <!-- italiano -->
+
+    [
+        "name" => "pizza margherita",
+        "price" => 12,
+        "description" => "Una deliziosa pizza con pomodoro, mozzarella e basilico.",
+        "ingredients" => ['impasto', 'pomodoro', 'mozzarella', 'basilico'],
+        "is_available" => true,
+        "cover_image" => "pizza_margherita.jpg",
+        
+    ],
+    [
+        "name" => "insalata mista",
+        "price" => 8,
+        "description" => "Un'insalata fresca con varietà di verdure di stagione.",
+        "ingredients" => ['lattuga', 'pomodoro', 'carote', 'cipolla', 'olive'],
+        "is_available" => true,
+        "cover_image" => "insalata_mista.jpg",
+        
+    ],
+    [
+        "name" => "spaghetti alla bolognese",
+        "price" => 14,
+        "description" => "Spaghetti con ragù di carne ricco e saporito.",
+        "ingredients" => ['spaghetti', 'carne macinata', 'pomodoro', 'cipolla', 'aglio'],
+        "is_available" => true,
+        "cover_image" => "spaghetti_bolognese.jpg",
+        
+    ],
+    [
+        "name" => "insalata di frutta",
+        "price" => 9,
+        "description" => "Un'insalata fresca e dolce con frutta di stagione.",
+        "ingredients" => ['mela', 'kiwi', 'fragole', 'ananas', 'uva'],
+        "is_available" => true,
+        "cover_image" => "insalata_di_frutta.jpg",
+       
+    ],
+    [
+        "name" => "sushi assortito",
+        "price" => 18,
+        "description" => "Una selezione di sushi fresco e prelibato.",
+        "ingredients" => ['riso', 'pesce crudo', 'alga nori', 'wasabi', 'soia'],
+        "is_available" => true,
+        "cover_image" => "sushi_assortito.jpg",
+        
+    ],
+
+    // <!-- giapponese -->
+
+    [
+    "name" => "Sushi Misto",
+    "price" => 20,
+    "description" => "Un assortimento di sushi fresco con nigiri, sashimi e maki preparato con pesce di alta qualità.",
+    "ingredients" => ['riso', 'pesce crudo', 'alghe nori', 'wasabi', 'zenzero marinato', 'soia'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Ramen di Maiale Tonkotsu",
+    "price" => 15,
+    "description" => "Una deliziosa ciotola di ramen con brodo ricco di maiale Tonkotsu, tagliatelle, cipolla verde e uovo sodo.",
+    "ingredients" => ['tagliatelle', 'brodo di maiale Tonkotsu', 'cipolla verde', 'uovo sodo'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+
+],
+
+[
+    "name" => "Tempura Mista",
+    "price" => 18,
+    "description" => "Tempura croccante e leggera con gamberi, verdure e salsa tempura.",
+    "ingredients" => ['gamberi', 'verdure', 'farina', 'uova', 'salsa tempura'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Teriyaki di Salmone",
+    "price" => 22,
+    "description" => "Salmone glassato con salsa teriyaki dolce, servito con riso e verdure alla griglia.",
+    "ingredients" => ['salmone', 'salsa teriyaki', 'riso', 'verdure alla griglia'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Okonomiyaki",
+    "price" => 16,
+    "description" => "Una pancake giapponese cuocia sulla piastra con cavolo, carne e condita con salsa okonomiyaki e maionese.",
+    "ingredients" => ['cavolo', 'carne', 'farina', 'uova', 'salsa okonomiyaki', 'maionese'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Unagi Don",
+    "price" => 24,
+    "description" => "Anguilla glassata con salsa dolce unagi servita su un letto di riso.",
+    "ingredients" => ['anguilla', 'salsa unagi', 'riso'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+// <!-- cinese -->
+
+[
+    "name" => "Anatra alla Pechinese",
+    "price" => 25,
+    "description" => "Anatra croccante servita con pancake, cipollotti, cetrioli e salsa hoisin.",
+    "ingredients" => ['anatra', 'pancake', 'cipollotti', 'cetrioli', 'salsa hoisin'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Ravioli al Vapore",
+    "price" => 12,
+    "description" => "Ravioli ripieni di carne di maiale e aromatizzati al vapore, serviti con salsa di soia.",
+    "ingredients" => ['pasta', 'carne di maiale', 'salsa di soia'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Maiale Agrodolce",
+    "price" => 18,
+    "description" => "Piatto classico con carne di maiale fritta in salsa agrodolce con peperoni e ananas.",
+    "ingredients" => ['carne di maiale', 'salsa agrodolce', 'peperoni', 'ananas'],
+    "is_available" => true,
+    "cover_image" => "",
+   
+],
+
+[
+    "name" => "Zuppa di Wanton",
+    "price" => 10,
+    "description" => "Zuppa avvolgente con wanton ripieni di carne di maiale, noodles e verdure.",
+    "ingredients" => ['brodo', 'wanton', 'carne di maiale', 'noodles', 'verdure'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Pollo Kung Pao",
+    "price" => 16,
+    "description" => "Pollo saltato in padella con arachidi, peperoncino, peperoni e cipollotti.",
+    "ingredients" => ['pollo', 'arachidi', 'peperoncino', 'peperoni', 'cipollotti'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Bistecca di Manzo con Broccoli",
+    "price" => 22,
+    "description" => "Bistecca di manzo saltata con broccoli in una salsa ricca e saporita.",
+    "ingredients" => ['bistecca di manzo', 'broccoli', 'salsa'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+// <!-- messicano -->
+
+[
+    "name" => "Tacos al Pastor",
+    "price" => 15,
+    "description" => "Tacos con carne di maiale marinata, ananas, cipolla e coriandolo.",
+    "ingredients" => ['tortilla di mais', 'carne di maiale marinata', 'ananas', 'cipolla', 'coriandolo'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+
+],
+
+[
+    "name" => "Guacamole con Nachos",
+    "price" => 12,
+    "description" => "Avocado fresco schiacciato con pomodoro, cipolla, lime e servito con nachos croccanti.",
+    "ingredients" => ['avocado', 'pomodoro', 'cipolla', 'lime', 'nachos'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Enchiladas di Pollo",
+    "price" => 18,
+    "description" => "Tortillas di mais ripiene di pollo, formaggio e salsa, gratinate al forno.",
+    "ingredients" => ['tortilla di mais', 'pollo', 'formaggio', 'salsa'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Quesadillas con Guarnizione di Fagioli",
+    "price" => 16,
+    "description" => "Tortillas ripiene di formaggio, fagioli neri e guarnite con pico de gallo.",
+    "ingredients" => ['tortilla', 'formaggio', 'fagioli neri', 'pico de gallo'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Burrito di Carne",
+    "price" => 20,
+    "description" => "Burrito generoso con carne alla griglia, riso, fagioli, formaggio e salsa.",
+    "ingredients" => ['tortilla', 'carne alla griglia', 'riso', 'fagioli', 'formaggio', 'salsa'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Chiles Rellenos",
+    "price" => 22,
+    "description" => "Peperoni verdi ripieni di formaggio, carne e salsiccia, fritti e serviti con salsa di pomodoro.",
+    "ingredients" => ['peperoni verdi', 'formaggio', 'carne', 'salsiccia', 'salsa di pomodoro'],
+    "is_available" => true,
+    "cover_image" => "",
+  
+],
+
+// <!-- coreano -->
+
+[
+    "name" => "Bibimbap",
+    "price" => 16,
+    "description" => "Una miscela di riso, verdure saltate, carne, uovo fritto e gochujang (salsa piccante).",
+    "ingredients" => ['riso', 'verdure', 'carne', 'uovo', 'gochujang'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Kimchi Jjigae",
+    "price" => 14,
+    "description" => "Zuppa di kimchi piccante con carne, tofu e verdure, servita bollente.",
+    "ingredients" => ['kimchi', 'carne', 'tofu', 'verdure'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Bulgogi",
+    "price" => 18,
+    "description" => "Carne marinata (solitamente manzo) grigliata con cipolla e servita con riso.",
+    "ingredients" => ['carne marinata', 'cipolla', 'riso'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Japchae",
+    "price" => 20,
+    "description" => "Noodles di patata dolce saltati con verdure, carne e salsa di soia dolce.",
+    "ingredients" => ['noodles di patata dolce', 'verdure', 'carne', 'salsa di soia'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Tteokbokki",
+    "price" => 15,
+    "description" => "Bastoncini di riso al vapore in una salsa dolce e piccante a base di gochujang.",
+    "ingredients" => ['tteok (bastoncini di riso)', 'gochujang'],
+    "is_available" => true,
+    "cover_image" => "",
+    
+],
+
+[
+    "name" => "Samgyeopsal",
+    "price" => 22,
+    "description" => "Pancia di maiale cruda grigliata e servita con foglie di lattuga, aglio e ssamjang.",
+    "ingredients" => ['pancia di maiale', 'foglie di lattuga', 'aglio', 'ssamjang'],
+    "is_available" => true,
+    "cover_image" => "",
+   
+],
+
+
+
+
+];
