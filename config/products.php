6b5a01e131d38f2f5dbd9db49194ebe8220f288f
Valentina-Web-Developer <valentina.webdev@gmail.com>
2023-12-05 18:55:17 +0100

    fixed image seeder

--- config/products.php
+++ config/products.php
@@ -11,7 +11,7 @@ return [
         "description" => "Questo hamburger è un connubio perfetto tra il sapore delle carni scelte e le salse.",
         "ingredients" => ['pane', 'hamburger', 'maionese', 'ketchup', 'patatine', 'lattuga'],
         "is_available" => true,
-        "cover_image" => "",
+        "cover_image" => "https://previews.dropbox.com/p/thumb/ACH4RkAG3Ashz8GSArJJQOOKSXWcOxYxnEf3mMQOBMeW-hJW4katmoiIDsPB1pHI1QgasVHAm0BUImuuHzaarXoU2cqr556BHgrMIoIoZ5Sw56orXZKKCP5d6gXmH6-_jnS4YhbsIUzXc11Nk1Ekxc2By2XimAb_LDk_gJfstPGbz2F4QkgZUMBslNhzNJsIielPLagjDhQUYf3bmXWvczscWrlxIciszSTzxYCHPQ8Cceh8tcZAbsEkbqPo-SXoHL_K12NUfhxeooEWRP5ZP-EWy99kutLHRpGEZDz0xPyT5uenmiDfqARdYwiohJtV8nPMtJE-svZY_n67GA1PGuME/p.jpeg",
         
     ],
     [
@@ -20,7 +20,7 @@ return [
     "description" => "Un classico intramontabile americano con carne succulenta, formaggio fuso e la perfetta combinazione di condimenti.",
     "ingredients" => ['panino', 'polpetta di manzo', 'formaggio cheddar', 'ketchup', 'senape', 'sottaceti'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACEPiF3sErCI21cXjpA34zu9hY5CMsc5YA5IzC7RRNMDKQOg_bfsE691afmvZakw2Ezv89LfRkS1_OQMdh9vePwyQ8N8MA-NcA8yN3P1guNeM7bd_n8fmXdrGMvCKMRR_9OgaxC54H7O_NfgTb5lYHNpDPxMMmQwp4RePLmNAxuNZCvbqAJ6sUEMxeFsJjcdzt76d3cfdFGLzzOd738aJ6WDGWNqPY9Uxfo_GAnpSgM8lL1HdNedFN7e6CPD5qyhlavPCwptSmdg3Ntf09530J_6zDI2A7zQl0mlU24WW-t04tsk0N7VFZccvLEySQCq_8zmmlZlTDv9AsUufjVFCTji/p.jpeg",
     
 ],
 
@@ -30,7 +30,7 @@ return [
     "description" => "Maiale tirato tenero in una ricca salsa barbecue, servito in un morbido panino.",
     "ingredients" => ['panino', 'maiale tirato', 'salsa barbecue', 'coleslaw'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACHdBfMuhWB2glkQGA0b_HmIq0lcAkYKDK7AS1mDF_CZqlVwsTNuKBuNiLAbjqyIFpbJt_020fBWL4QXWCceYCAArWMzcfdshkMl-tLnGCORob-OQqVADizV7GqWu4CnUA2f-u8NbuOhiZJEL2CDEB5QbWdl7Mkx-O2kjvxsVAOruV36zt2ydZbj-5ljFa7mB4nVxLAGENj8w6oui9vfDiyCY11bVvuMP7vdVoxhrWZX1Tl8rcLzYVsF0sDdXqdzkja-ZuqqHLjtloYCFgk2JNTmtwISGPfHbho0tozweIcuaRgJc_Hq9_ffzeJl_9V1O3DvgMvneKi8iNQUgU6_ukeb/p.jpeg",
     
 ],
 
@@ -40,7 +40,7 @@ return [
     "description" => "Ali di pollo croccanti e piccanti, immerse in una salsa Buffalo agrodolce, perfette da condividere.",
     "ingredients" => ['ali di pollo', 'salsa Buffalo', 'salsa al formaggio blu', 'sedano'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://wips.plug.it/cips/buonissimo.org/cms/2023/11/progetto-senza-titolo-10-1.png?w=713&a=c&h=407",
     
 ],
 
@@ -50,7 +50,7 @@ return [
     "description" => "Una fetta della Grande Mela con una crosta sottile e pieghevole e deliziosi condimenti.",
     "ingredients" => ['impasto per pizza', 'salsa di pomodoro', 'formaggio mozzarella', 'salame piccante', 'olive'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://cdn.broadsheet.com.au/cache/dc/28/dc28a2c2df93faa89f96b899a103a3c4.jpg",
     
 ],
 
@@ -60,7 +60,7 @@ return [
     "description" => "Pollo croccante e dorato con un tocco di spezie del sud.",
     "ingredients" => ['pezzi di pollo', 'latticello', 'farina', 'spezie', 'purè di patate', 'sugo'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/02/Pollo-fritto.jpg",
     
 ],
 
@@ -70,7 +70,7 @@ return [
     "description" => "Un'insalata rinfrescante con pollo alla griglia, avocado, pancetta e formaggio blu.",
     "ingredients" => ['insalata mista', 'pollo alla griglia', 'avocado', 'pancetta', 'formaggio blu', 'condimento'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACE-SngiFxJM5_1c9J6kCVd02yqvClfbLIX6z_5sEKVC9hamvXCClpfQh8aZXKxYBO5WF4_HeBekZYf-SehTYVssw9AeiYRP6qiaFXdgsrT3NyQxTxf09ejyxoI3Vq4Y6kLu1gIaFCcCVLS0141zPb5SiAWQs0bcrjvs0J566rtv3VnWXUmgcHeu0gw8YxPH_yGb0RGRyB4GThHM3t-ro2ay9MP1J4tQdLELuZpnG8WvaXt11r604Y8A-lsQuffrytDmwe0wQUziHxDMufMr8XZLlT4FOokuqc1A9ZSHDna0bZy8OESDwBLTlpla2ZaGKO7sHzs-HIA2IC-KPZyYtQ_m/p.jpeg",
     
 ],
 
@@ -82,7 +82,7 @@ return [
         "description" => "Una deliziosa pizza con pomodoro, mozzarella e basilico.",
         "ingredients" => ['impasto', 'pomodoro', 'mozzarella', 'basilico'],
         "is_available" => true,
-        "cover_image" => "pizza_margherita.jpg",
+        "cover_image" => "https://images.prismic.io/eataly-us/ed3fcec7-7994-426d-a5e4-a24be5a95afd_pizza-recipe-main.jpg?auto=compress,format",
         
     ],
     [
@@ -91,7 +91,7 @@ return [
         "description" => "Un'insalata fresca con varietà di verdure di stagione.",
         "ingredients" => ['lattuga', 'pomodoro', 'carote', 'cipolla', 'olive'],
         "is_available" => true,
-        "cover_image" => "insalata_mista.jpg",
+        "cover_image" => "https://media-assets.lacucinaitaliana.it/photos/64b93e4dda53b8e5e1b61301/1:1/w_2240,c_limit/Insalata-mista-al-mirin-e-crostini-di-Altamura.jpg",
         
     ],
     [
@@ -100,7 +100,7 @@ return [
         "description" => "Spaghetti con ragù di carne ricco e saporito.",
         "ingredients" => ['spaghetti', 'carne macinata', 'pomodoro', 'cipolla', 'aglio'],
         "is_available" => true,
-        "cover_image" => "spaghetti_bolognese.jpg",
+        "cover_image" => "https://previews.dropbox.com/p/thumb/ACFlVyMYp7uABqdZcZOcSOut5VUp4Q7iyEUn2X3i6NXsA5ed6sztQu8ReaIo15iU-rr5OG_TSepuledi7P98StsuCklvIqbBBXgEIwDvIPYqcFhB_vxs8jriTMAZPMy6ozTwljxP4MC_ErjYab0_w1j1tB7EuoYNBlELFmkSXibbpi_qZWASqee8FWlqJyUlKLxFGxAd6b9zijbm1fGquFIfZO_aQSCTNHvE3iJ7xhdVLxQfWNs8BYNPRezIV5GQwex0hWqCLw3Bn1Ir-OjF-xRQOmzs5r66pgi3a21I_bBBEfI5MZO_FLRPPJklWyRSH3oCzfm4KE9RpvCo1d0NPoARUr-4Sg5fweeAcRIpDjRB3fvuMLDm4sNouj2w_TBhabBiPJBbDhg10Z_DVB69MUf9GFeWgNPvwq8ZywC2HFcEdpuV_4vHdBVRj-8q1XMlM-oXEISuYl5_kBLhOXp2HiO9/p.jpeg",
         
     ],
     [
@@ -109,17 +109,8 @@ return [
         "description" => "Un'insalata fresca e dolce con frutta di stagione.",
         "ingredients" => ['mela', 'kiwi', 'fragole', 'ananas', 'uva'],
         "is_available" => true,
-        "cover_image" => "insalata_di_frutta.jpg",
+        "cover_image" => "https://www.buttalapasta.it/wp-content/uploads/2009/07/macedonia-esotica.jpg",
        
-    ],
-    [
-        "name" => "sushi assortito",
-        "price" => 18,
-        "description" => "Una selezione di sushi fresco e prelibato.",
-        "ingredients" => ['riso', 'pesce crudo', 'alga nori', 'wasabi', 'soia'],
-        "is_available" => true,
-        "cover_image" => "sushi_assortito.jpg",
-        
     ],
 
     // <!-- giapponese -->
@@ -130,7 +121,7 @@ return [
     "description" => "Un assortimento di sushi fresco con nigiri, sashimi e maki preparato con pesce di alta qualità.",
     "ingredients" => ['riso', 'pesce crudo', 'alghe nori', 'wasabi', 'zenzero marinato', 'soia'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACE4qN2Gki9IZdLylpcKCaS_6GsCmdiQr2eUrTYpzgnhj57i42VNre7YCDzwpbOOeDxapyKvpHRcgjF1-5z0WXjlPsfPwlVkpMWjByrrDANwmnrloyHiMQLzzzfneyW6NIv_piL4OBlu_6rJ__8UXcb5eNH-RHeAyhEqvJg1DGpgHKAh5BP3EoH8O-xhepCIC7e5lEBX5vovlpeKGsYY9rqiCHlXMCEl3Ot7eG_JkkSfVXH5nFmYZOkA4TNerGXcCNHz-SOMAsP1VPO_O5NhqySHyRQb7gAhqYNuH3WFOmbxDISKzjCdMfzSGaYkySRe7J-q3VBzyjFYyIrRXjWrNET1xdT-jaAuzyWB6pXAWxjqDw/p.jpeg",
     
 ],
 
@@ -140,7 +131,7 @@ return [
     "description" => "Una deliziosa ciotola di ramen con brodo ricco di maiale Tonkotsu, tagliatelle, cipolla verde e uovo sodo.",
     "ingredients" => ['tagliatelle', 'brodo di maiale Tonkotsu', 'cipolla verde', 'uovo sodo'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACEiaNZb9OtQwrH5Z67dmF8cCqSOen0sL2EepZF7hB20IrHFnufir2v3mm8D_focqunal3DZVHEQOSTcEGAB0lBs3jEUzGheLVsELkQKYRbzn-Oirv4JMXettxjdRASgCWBsflVQ584xJA3dJ7MTEg2kygYGvFFFOjuvFRmb0NkDAnDpoWOtRw7U_zVEnHA-vKROByXT2vxtD8jy0SoSTk87A1sMuYk92ICQVoGO5iGq0uxPOxufWhV77AM3V0yYGM47y9QEoxoVIdFUw2sfUa6ADF4s7iSEV3bduD2aqculuIRSK2wvdeJ6Kffv8h7FOg2nFxJ4N4r_CLrHqRGwUmsY/p.jpeg",
     
 
 ],
@@ -151,7 +142,7 @@ return [
     "description" => "Tempura croccante e leggera con gamberi, verdure e salsa tempura.",
     "ingredients" => ['gamberi', 'verdure', 'farina', 'uova', 'salsa tempura'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/04/11-tempura-di-verdure-servi__MG_6906-1200x675.jpg",
     
 ],
 
@@ -161,7 +152,7 @@ return [
     "description" => "Salmone glassato con salsa teriyaki dolce, servito con riso e verdure alla griglia.",
     "ingredients" => ['salmone', 'salsa teriyaki', 'riso', 'verdure alla griglia'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2019/12/Salmone-in-salsa-teriyaki-0D6A6851-1200x675.jpg",
     
 ],
 
@@ -171,7 +162,7 @@ return [
     "description" => "Una pancake giapponese cuocia sulla piastra con cavolo, carne e condita con salsa okonomiyaki e maionese.",
     "ingredients" => ['cavolo', 'carne', 'farina', 'uova', 'salsa okonomiyaki', 'maionese'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://www.spoonforkbacon.com/wp-content/uploads/2021/09/okonomiyaki-1153x1536.jpg",
     
 ],
 
@@ -181,7 +172,7 @@ return [
     "description" => "Anguilla glassata con salsa dolce unagi servita su un letto di riso.",
     "ingredients" => ['anguilla', 'salsa unagi', 'riso'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://sudachirecipes.com/wp-content/uploads/2022/07/unagi-don-sqr.jpg",
     
 ],
 
@@ -193,7 +184,7 @@ return [
     "description" => "Anatra croccante servita con pancake, cipollotti, cetrioli e salsa hoisin.",
     "ingredients" => ['anatra', 'pancake', 'cipollotti', 'cetrioli', 'salsa hoisin'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://wips.plug.it/cips/buonissimo.org/cms/2012/03/anatra-alla-pechinese.jpg?w=713&a=c&h=407",
     
 ],
 
@@ -203,7 +194,7 @@ return [
     "description" => "Ravioli ripieni di carne di maiale e aromatizzati al vapore, serviti con salsa di soia.",
     "ingredients" => ['pasta', 'carne di maiale', 'salsa di soia'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/vimdb/170418.jpg",
     
 ],
 
@@ -213,7 +204,7 @@ return [
     "description" => "Piatto classico con carne di maiale fritta in salsa agrodolce con peperoni e ananas.",
     "ingredients" => ['carne di maiale', 'salsa agrodolce', 'peperoni', 'ananas'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://wips.plug.it/cips/buonissimo.org/cms/2012/06/maiale-in-agrodolce.jpg",
    
 ],
 
@@ -223,7 +214,7 @@ return [
     "description" => "Zuppa avvolgente con wanton ripieni di carne di maiale, noodles e verdure.",
     "ingredients" => ['brodo', 'wanton', 'carne di maiale', 'noodles', 'verdure'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://wips.plug.it/cips/buonissimo.org/cms/2012/03/zuppa-di-won-ton.jpg?w=713&a=c&h=407",
     
 ],
 
@@ -233,7 +224,7 @@ return [
     "description" => "Pollo saltato in padella con arachidi, peperoncino, peperoni e cipollotti.",
     "ingredients" => ['pollo', 'arachidi', 'peperoncino', 'peperoni', 'cipollotti'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://primochef.it/wp-content/uploads/2022/04/SH_pollo_in_agrodolce.jpg",
     
 ],
 
@@ -243,7 +234,7 @@ return [
     "description" => "Bistecca di manzo saltata con broccoli in una salsa ricca e saporita.",
     "ingredients" => ['bistecca di manzo', 'broccoli', 'salsa'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://www.ricettasprint.it/wp-content/uploads/2021/03/Manzo-e-broccoli-saltati-alla-cantonese-ricettasprint.jpg",
     
 ],
 
@@ -255,7 +246,7 @@ return [
     "description" => "Tacos con carne di maiale marinata, ananas, cipolla e coriandolo.",
     "ingredients" => ['tortilla di mais', 'carne di maiale marinata', 'ananas', 'cipolla', 'coriandolo'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACE2vJxkWVrtWmlro3MPmtbKl7N6XxDya0tHs7nl3MdbfrR99-UKqlZcFmU3tF76A84GKK4rELxDaGixTu_jRrLqS4VbCCiUxB1mwQNWuNhr3MdE2iPjA95BpznlZgIefsYTJ4omDwRG3PPc8sKPxU7mBj4qjhCgiFkAaJyJpcmbae3uZrKCrGN20h1tQoVRuy1ioYlB6E8DWCNsXv8f9poLtZ6y1LZmx75sD3-mMWeg-9xIQ4unJfI3VdB9rr-lWyv2QGpQ0pgh_Pjlawkn1FaUUwODKo5zsDnY5EL8dChnkfvaP0U7PnlrRQw3_rW5c9AtuXSEVmoJHfCg_SqZXLDS/p.jpeg",
     
 
 ],
@@ -266,7 +257,7 @@ return [
     "description" => "Avocado fresco schiacciato con pomodoro, cipolla, lime e servito con nachos croccanti.",
     "ingredients" => ['avocado', 'pomodoro', 'cipolla', 'lime', 'nachos'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://veggiesituation.com/wp-content/uploads/2023/06/nachos-con-guacamole-e-salsa-pico-de-gallo-1.jpg",
     
 ],
 
@@ -276,7 +267,7 @@ return [
     "description" => "Tortillas di mais ripiene di pollo, formaggio e salsa, gratinate al forno.",
     "ingredients" => ['tortilla di mais', 'pollo', 'formaggio', 'salsa'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACGSZswWCVaLb44WsEAe4IDOZ_Wa2l54bk2gB628jUyGIFGkfxpIEJBtxM6lHnjTqY2zEEqUqwJmAaYMJwphvjWeq_S69DxZ_IKt938TuhDVw-PRqWLz4498Nytu_FC_Gbj_JIEXAvJlIncjk1LujZe_t_FY7TwJXXf1zg661QV2cSQjpVhKxt8xpUai14oXFJ6sbTkxrOIdJTEESx-_AIMJPpucX_NNOXsIjDiTLem9BgROIKmMo8fU6pZjETdvE5UlCYQw4TvJRuYsyX1lK9taxP0HzevgYA-CQqVXd6bN8phIwQBpYg5lsCae0OV2jpfQA02oV0tb4kojt71jFmxr/p.jpeg",
     
 ],
 
@@ -286,7 +277,7 @@ return [
     "description" => "Tortillas ripiene di formaggio, fagioli neri e guarnite con pico de gallo.",
     "ingredients" => ['tortilla', 'formaggio', 'fagioli neri', 'pico de gallo'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACE60C5g-LA3uKa1KYPC6o60mP7Y2Oz79OVOqJ4ff_CB592MHoRHSwB4eiLWmog8WC-Ac8Xr9aXKfqk2YejuKz8AIN05EZ7vkFhNc5f6tWvx-Q2PCXVx4S7_t9I3bVwgWExCOdoqaGTY11P4nlbRnwwCmNqbsaltDTmD9GyvfN0CRfqqHFk10-L_EHazlgH-AzIZbffeR7bepx_3z8blsTBX9hsTyHdW7OyEQQkyiZvKeCaoe1FyHKUTdJVl1sRHjJsX0VXN9rFkEeu8V_P-AeD7-HkVjJrnLzQEG8QM_vgliFFPpoosk29REhdmCaQA3UxGFKB-e8DJAkMwoXpHVCqW/p.jpeg",
     
 ],
 
@@ -296,7 +287,7 @@ return [
     "description" => "Burrito generoso con carne alla griglia, riso, fagioli, formaggio e salsa.",
     "ingredients" => ['tortilla', 'carne alla griglia', 'riso', 'fagioli', 'formaggio', 'salsa'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACE4aTuPr_Zqq8Aq5uDssdjjrq4FQGOqlA9tSZ9eVnapz57TtL4mwLYal_UjkrlBMbl6cifTrZn3uaXsMjVmJJWlWQRYmZBiySjyl_M-Y57Nv80XtxW_t57EMFrpa0DhXIikr0_opYckJp0bdTvIlda_mNEW7R4U6nF7QHEk6Xy3etSO_-51M540O3KoY9kJB8k164GWVUNhuBIRfQFPHsD7CIWcUhn52SVqYoPDw8fnrVbQ7Qga542eQlJ-Ae3XYBxsugYfe9g6j9CwdFQ0kKyIw6ew1y-qumTPdsANMcDtx3ShdLLtxwCIwyw9Y9LYHu0w61oaK0zJ-hDnOy2h3xpa/p.jpeg",
     
 ],
 
@@ -306,7 +297,7 @@ return [
     "description" => "Peperoni verdi ripieni di formaggio, carne e salsiccia, fritti e serviti con salsa di pomodoro.",
     "ingredients" => ['peperoni verdi', 'formaggio', 'carne', 'salsiccia', 'salsa di pomodoro'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://cucinareamodomio.files.wordpress.com/2020/08/img_20200821_1324117692.jpg?w=1024",
   
 ],
 
@@ -318,7 +309,7 @@ return [
     "description" => "Una miscela di riso, verdure saltate, carne, uovo fritto e gochujang (salsa piccante).",
     "ingredients" => ['riso', 'verdure', 'carne', 'uovo', 'gochujang'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACFA1jYKk8Sn89Ln9w0J9UM4cFI5icrkHVfDhaVpa5iEtduVK7TDy9qdzPx18zGayFW060MpvqPvnMqz8WlkgYlUegoPhSMRueFpxqKS6yVPzpOKdEB7QkbVg1ll6LbiAViIiPqoRRp0aJbCwKWE9FsMqr1rFxE224DsnFCdj4HwgUaTy4q0AJa7QIY2krUObMPg6j-KujLivqI_QMRzOgf-lswTO_DTs_UUwTh0XrFHcXJKfzqRJPUtH-s1K9xorEQeuQmD7NBOMRpcSwXgWeDW-8Px2lzdAsCG0xEK-XeQVBUgFA-S44if61q-gJrLun5HL6qn1R3DarMchG35vOgWHhkQ65QP6ik_XEJM3niYWA/p.jpeg",
     
 ],
 
@@ -328,7 +319,7 @@ return [
     "description" => "Zuppa di kimchi piccante con carne, tofu e verdure, servita bollente.",
     "ingredients" => ['kimchi', 'carne', 'tofu', 'verdure'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACHLhfMNt2aia6DTOLyjvzzqdQ3f-hTbuS204xkWHcAmrAqYNgsV481wTEo85c3sLsElhndc_rOntuiYDLg1ps0pJR52sWNDkDEhwoJN48yHv_tq92qYXK7fPebIj5KX6sZ8mGnAMrE2__su3P-sSnvF0l-cHpl28XahosTY_pPYYq9AXXxiQtZy3SleweWBOWXkfkAF5bakMmjUSdoIq0AwuXkyjg2x52i-WdYa1ou5oKidWu-lcPneIoLDB3jPZAUdYdppYAyFzsbTP2wrqqFpG-ijQVuDXJfSfGwXi8bx_ECzUCoA_gz4NNC7t6R4YPGZ6QOrs1lUCMFY2vorsRy7/p.jpeg",
     
 ],
 
@@ -338,7 +329,7 @@ return [
     "description" => "Carne marinata (solitamente manzo) grigliata con cipolla e servita con riso.",
     "ingredients" => ['carne marinata', 'cipolla', 'riso'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://www.beyondkimchee.com/wp-content/uploads/2023/08/beef-bulgogi-thumbnail.jpg",
     
 ],
 
@@ -348,7 +339,7 @@ return [
     "description" => "Noodles di patata dolce saltati con verdure, carne e salsa di soia dolce.",
     "ingredients" => ['noodles di patata dolce', 'verdure', 'carne', 'salsa di soia'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://assets.epicurious.com/photos/614b77f992c8a88b430d0a2f/4:3/w_3024,h_2268,c_limit/Japchae1-jd.jpg",
     
 ],
 
@@ -358,7 +349,7 @@ return [
     "description" => "Bastoncini di riso al vapore in una salsa dolce e piccante a base di gochujang.",
     "ingredients" => ['tteok (bastoncini di riso)', 'gochujang'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://drivemehungry.com/wp-content/uploads/2021/02/tteokbokki-korean-rice-cakes-7.jpg",
     
 ],
 
@@ -368,7 +359,7 @@ return [
     "description" => "Pancia di maiale cruda grigliata e servita con foglie di lattuga, aglio e ssamjang.",
     "ingredients" => ['pancia di maiale', 'foglie di lattuga', 'aglio', 'ssamjang'],
     "is_available" => true,
-    "cover_image" => "",
+    "cover_image" => "https://previews.dropbox.com/p/thumb/ACHS9NqFwWT9aLiHja9Qwkn8gSm2EetZ6066E-6WxyVC6V65SGfVcxe0FPNlvDbdOq6Imv24wc1BcUdOfGTJH057WZ-m9aF-tkt2PyaAbEAUj8Ux73b5oNhUK87mjRqwJaTCkxPjOwh9jXwdD-s561Do3L7aYF12fIbQYQGlSC4NbEGIUjVNnVhWILh3SXl0JE89v6sQRaCrb4UElJpaDBXV9JlVue1glf_SK1WpX3sqXXSthUc9c-2Jz6yrElXWFlrT6tp-PCEG6_5PKa8YMtIa-CxU_Iv0urC7IdCbMRgLvX5gcu_xq8-MjqhXF7bUtWZMWZcM4dzuD2PthP-IbCXt/p.jpeg",
    
 ],
 
