91fd833007c85c974d1fff1a4d6e6990f40f5903
EsterDahri <esterdahri@gmail.com>
2023-12-07 09:14:05 +0100

    add variables

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -35,63 +35,73 @@
                             <tr class="table-primary">
                                 <td class="text-center m-auto" scope="row">{{ $product->id }}</td>
                                 <td class="text-center m-auto">{{ $product->name }}</td>
-                                <td class="text-center m-auto">
-                                    <img class="img-fluid w-50" src="{{ $product->cover_image }}" alt="">
-                                </td>
+                                @if ($product->cover_image){
+                                    @if (Str::startsWith('https')){
+                                        <td class="text-center m-auto">
+                                            <img class="img-fluid w-50" src="{{ $product->cover_image }}" alt="">
+                                        </td>
+                                        }
+                                    @else
+                                        }
+                                    @else{
+                                        There's no image available
+                                        }
 
-                                <td class="text-center m-auto">
-                                    <div class="d-flex">
+                                        <td class="text-center m-auto">
+                                            <div class="d-flex">
 
-                                    <a class="btn btn-success" href="">
-                                        <i class="fa-solid fa-eye text-white"></i>
-                                    </a>
-                                    <a class="btn btn-warning mx-2" href="">
-                                        <i class="fa-solid fa-pencil text-white"></i></a>
-                                    <!-- Modal trigger button -->
-                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                        data-bs-target="#modalId-{{ $product->id }}">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
+                                                <a class="btn btn-success" href="">
+                                                    <i class="fa-solid fa-eye text-white"></i>
+                                                </a>
+                                                <a class="btn btn-warning mx-2" href="">
+                                                    <i class="fa-solid fa-pencil text-white"></i></a>
+                                                <!-- Modal trigger button -->
+                                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                                    data-bs-target="#modalId-{{ $product->id }}">
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </button>
 
-                                    <!-- Modal Body -->
-                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                    <div class="modal fade" id="modalId-{{ $product->id }}" tabindex="-1"
-                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                        aria-labelledby="modalTitleId" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                            role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="modalTitleId">Cancella</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
+                                                <!-- Modal Body -->
+                                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                                <div class="modal fade" id="modalId-{{ $product->id }}" tabindex="-1"
+                                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                                    aria-labelledby="modalTitleId" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                                        role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="modalTitleId">Cancella</h5>
+                                                                <button type="button" class="btn-close"
+                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Sei sicuro di cancellare questo piatto?
+                                                                {{ $product->name }}?
+                                                            </div>
+                                                            <div class="modal-footer justify-content-evenly">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-bs-dismiss="modal">Chiudi</button>
+                                                                <form action=" " method="post">
+                                                                    @csrf
+                                                                    @method('DELETE')
+
+                                                                    <button type="submit"
+                                                                        class="btn btn-danger">Cancella</button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="modal-body">
-                                                    Sei sicuro di cancellare questo piatto? {{ $product->name }}?
-                                                </div>
-                                                <div class="modal-footer justify-content-evenly">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Chiudi</button>
-                                                    <form action=" " method="post">
-                                                        @csrf
-                                                        @method('DELETE')
 
-                                                        <button type="submit" class="btn btn-danger">Cancella</button>
-                                                    </form>
-                                                </div>
+
+                                                <!-- Optional: Place to the bottom of scripts -->
+                                                <script>
+                                                    const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
+                                                </script>
                                             </div>
-                                        </div>
-                                    </div>
 
 
-                                    <!-- Optional: Place to the bottom of scripts -->
-                                    <script>
-                                        const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
-                                    </script>
-                                    </div>
-
-
-                                </td>
+                                        </td>
                             </tr>
                         @empty
                             <h1>Non hai ancora prodotti!</h1>
