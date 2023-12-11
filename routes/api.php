b7fd9bf1b2a9e207297a0a6c92bb7046cec8e2ee
RiccardoVoltolina <riky.volto@live.com>
2023-12-11 17:48:44 +0100

    try to implement api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
+use App\Models\Restaurant;
+use App\Models\Typology;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +22,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', function (Request $request) {
+    return response()->json([
+        'success' => true,
+        'result' => Product::all()
+    ]);
+});
+
+
+
+
+Route::get('restaurants', function () {
+    return response()->json([
+        'success' => true,
+        'result' => Restaurant::all()
+    ]);
+});
+
+
+
+// Route::get('restaurants/typology', function () {
+//     return response()->json([
+//         'success' => true,
+
+//         // Ottieni il ristorante associato all'utente autenticato
+//         $restaurants = Auth::user()->restaurant,
+
+//         // Ottieni l'utente autenticato
+//         $user = Auth::user(),
+
+
+//         'result' => Auth::user()->$restaurants->typologies,
+
+//     ]);
+// });
