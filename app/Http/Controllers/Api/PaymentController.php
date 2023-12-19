abe0df14f0e3773441010b7bba855669ee1bc9f9
RiccardoVoltolina <riky.volto@live.com>
2023-12-19 22:10:49 +0100

    send amount on paypal

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,34 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    protected $gateway;
 
-    public function __construct(Gateway $gateway)
-    {
-        $this->gateway = $gateway;
-    }
+    public function generate(Request $request, Gateway $gateway) {
 
-    public function generate(Request $request) {
+        // dd($gateway->clientToken());
 
-
-
-        $token = $this->gateway->clientToken()->generate();
+        $token = $this->$gateway->clientToken()->generate();
 
         $data = [
+            'succes' => true,
             'token' => $token
         ];
         
         
         return response()->json($data, 200);
-        // return 'generate';
     }
 
-    public function makePayment(Request $request) {
+    public function makePayment(OrderRequest $request, Gateway $gateway) {
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if ($result->success) {
+            $data = [
+                'succes' => true,
+                'message' => 'Transazione eseguita con successo!'
+            ];
+            return response()->json($data,200);
+        } else {
+
+            $data = [
+                'succes' => false,
+                'message' => 'Transazione fallita!'
+            ];
+
+            return response()->json($data,401);
+        }
         return 'makePayment';
     }
 
