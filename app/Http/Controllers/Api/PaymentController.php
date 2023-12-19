2d0c6fb843ec1c9b9368a9f42b17bbd9627e8467
RiccardoVoltolina <riky.volto@live.com>
2023-12-19 20:45:21 +0100

    fix environment

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
+    protected $gateway;
+
+    public function __construct(Gateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
     public function generate(Request $request) {
-        return 'generate';
+
+
+
+        $token = $this->gateway->clientToken()->generate();
+
+        $data = [
+            'token' => $token
+        ];
+        
+        
+        return response()->json($data, 200);
+        // return 'generate';
     }
 
     public function makePayment(Request $request) {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Braintree;
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // $this->app->bind(Gateway::class, function ($app) {
+        //     return new Gateway([
+        //         'enviroment' => 'sandbox',
+        //         'merchantId' => 'bhcnjb7v5ps4kyg2',
+        //         'publicKey' => 'n78rfbjjfj9kfy4r',
+        //         'privateKey' => 'b5608ecc38630b78349f565d1f8425f2',
+        //     ]);
+        // });
     }
 
     /**
@@ -20,5 +29,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->app->singleton(Gateway::class, function ($app) {
+            return new Gateway(
+                [
+                    'environment' => 'sandbox',
+                    'merchantId' => 'bhcnjb7v5ps4kyg2',
+                    'publicKey' => '7vgr4pdhzpm72r8x',
+                    'privateKey' => 'f819a2ea9fc39c317ec41c26a383c443'
+                ]
+            );
+        });
     }
 }
